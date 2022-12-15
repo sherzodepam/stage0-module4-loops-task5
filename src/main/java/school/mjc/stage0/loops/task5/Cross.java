1d452b1034f5b2978d1510c7034361bc7eee5c7b
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-15 14:12:10 +0500

    fix

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        if (sideLength==0){
+            System.out.print("");
+            return;
+        }
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < sideLength; i++) {
+            if (sideLength / 2 != i)
+                for (int j = 0; j < sideLength; j++)
+                    if (j != sideLength / 2)
+                        s.append(" ");
+                    else s.append("8");
+            else
+                for (int j = 0; j < sideLength; j++)
+                    s.append("8");
+
+            if (i!=sideLength-1)
+                s.append("\n");
+        }
+        System.out.println(s);
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {//0
+            if (i<height/2) {
+                for (int j = 0; j < height; j++) {//0
+                    if (j >= i && j <= height - (i + 1))
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
+                }
+            } else{
+                for (int j = height; j >0; j--) {//3   0-5
+                    if (j > height - (i + 1) && j <= i+1)
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
